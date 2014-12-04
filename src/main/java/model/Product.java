468bfa01c7051da2e14f2bddcfcb4a1dddd8a0c5
andgra <andreas.graslund@bisnode.com>
2014-12-04 11:00:35 +0100

    Product uppdaterad

--- src/main/java/model/Product.java
+++ src/main/java/model/Product.java
@@ -1,6 +1,8 @@
 package model;
 
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * Created by andgra on 2014-10-09.
@@ -8,59 +10,43 @@ package model;
 
 public class Product {
 
-    private String id;
-    private String name;
-    private double priceIncVat;
-    private double vatPercentage;
-    private double vatAmount;
+    private final String id;
+    private final String name;
+    private final double priceIncVat;
+    private final double vatPercentage;
+    private final double vatAmount;
 
-    public Product(){
 
-    }
-    public Product(String id, String name, double priceIncVat, double vatPercentage, double vatAmount){
+    @JsonCreator
+    public Product(@JsonProperty("id") String id,@JsonProperty("name") String name, @JsonProperty("priceIncVat") double priceIncVat, @JsonProperty("vatPercentage") double vatPercentage, @JsonProperty("vatAmount") double vatAmount) {
         this.id = id;
         this.name = name;
         this.priceIncVat = priceIncVat;
         this.vatPercentage = vatPercentage;
         this.vatAmount = vatAmount;
     }
+
     public String getId() {
         return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public double getPriceIncVat() {
         return priceIncVat;
     }
 
-    public void setPriceIncVat(double priceIncVat) {
-        this.priceIncVat = priceIncVat;
-    }
 
     public double getVatPercentage() {
         return vatPercentage;
     }
 
-    public void setVatPercentage(double vatPercentage) {
-        this.vatPercentage = vatPercentage;
-    }
 
     public double getVatAmount() {
         return vatAmount;
     }
 
-    public void setVatAmount(double vatAmount) {
-        this.vatAmount = vatAmount;
-    }
 }
