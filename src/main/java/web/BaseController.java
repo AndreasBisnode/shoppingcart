76f4e811b13e33f8a27a45fe55f7dd27fc9f4b41
andgra <andreas.graslund@bisnode.com>
2014-12-04 11:26:28 +0100

    Logger

--- src/main/java/web/BaseController.java
+++ src/main/java/web/BaseController.java
@@ -1,7 +1,8 @@
 package web;
 
 import exception.ResourceNotFoundException;
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -10,10 +11,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
  * Created by andgra on 2014-11-21.
  */
 public abstract class BaseController {
-    Logger logger = (Logger) Logger.getInstance(getClass());
+    Logger logger = LoggerFactory.getLogger(this.getClass());
     @ExceptionHandler(ResourceNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public void handleError(ResourceNotFoundException e) {
-        logger.error(e);
+        logger.error(String.valueOf(e));
     }
 }
