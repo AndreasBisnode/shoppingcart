4e36bec3fa10370f28902d8f55aff094a1c0f91c
andgra <andreas.graslund@soliditet.se>
2014-12-08 08:51:23 +0100

    nytt försök

--- src/main/java/web/Application.java
+++ src/main/java/web/Application.java
@@ -2,12 +2,15 @@ package web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
-@ComponentScan(basePackages = {"repository.*", "web.*","*","mongoDb.*"})
+@ComponentScan(basePackages = {"repository.*", "web.*","*","mongo.*"})
 @EnableAutoConfiguration
+
 public class Application {
 
     public static void main(String[] args) {
